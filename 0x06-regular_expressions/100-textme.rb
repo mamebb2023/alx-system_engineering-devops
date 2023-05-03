bf5ac908a04cff5634daab24202a983856e0dd2a
Mohammednur <mamebb2023@gmail.com>
2023-05-03 16:49:56 +0300

    Logs

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[(.*?)\]\[(.*?)\]\[(.*?)\]/).join(",")
+from = ARGV[0].scan(/from:(.\w+)/).join
+to = ARGV[0].scan(/to:(.\d+)/).join
+flags = ARGV[0].scan(/flags:(.+-\d)/).join
+puts "#{from},#{to},#{flags}"
